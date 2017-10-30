7b777e3349063dc2b042178145327484f626bed5
vitor.gamboa <vitor.gamboa@RJOD212243.GOV>
2017-10-30 11:48:14 -0200

    Primeiro commit

---
+++ src/br/com/cursojsf/bean/OlaBean.java
@@ -0,0 +1,23 @@
+package br.com.cursojsf.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class OlaBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
